b6b01681de0a0f28aa9adc675df7850274a655f7
Quentin J <quentinjoubert123@gmail.com>
2020-07-24 11:26:09 +0200

    Initial Commit

---
+++ webpack.config.js
@@ -0,0 +1,39 @@
+
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require("path");
+
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist")
+  },
+  mode: "development",
+  devServer: {
+    contentBase: "./dist"
+  },
+  devtool: "inline-source-map",
+  module: {
+    rules: [
+      {
+        test: /\.html/,
+        loader: "html-loader",
+        options: {
+          minimize: true
+        }
+      },
+      {
+        test: /\.css/,
+        use: [
+          "style-loader",
+          "css-loader"
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/index.html"
+    })
+  ]
+};
